6fca5161ea7f669fe1d61ad67b0d5fc5ea545064
ArcadG <puly-66@mail.ru>
2019-11-21 14:55:50 +0300

    answer_2

--- answer_2/the_equation.rb
+++ answer_2/the_equation.rb
@@ -3,16 +3,16 @@ print "ax^+bx+c=0 Введите данные a,b,c:" ""
 value = gets.chomp.split(",").map(&:to_f)
 a, b, c = value
 
-d = b ** 2 - 4 * a * c
+d = b**2 - 4 * a * c
 
 if d < 0
   print "Корней нет"
 elsif d > 0
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
-  print "Корни x1=#{x1} x2=#{x2}"    
+  x1 = (-b + Math.sqrt(d))/(2 * a)
+  x2 = (-b - Math.sqrt(d))/(2 * a)
+  print "Корни x1 = #{x1} x2 = #{x2}"    
 else d = 0
-  x = -b / (2 * a)
-  print "Корень один x=#{x}" 
+  x = -b/(2 * a)
+  print "Корень один x = #{x}" 
 end
 
