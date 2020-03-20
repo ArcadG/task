4e00f97d0380661a6db2726090ac75cb572d9e33
ArcadG <puly-66@mail.ru>
2020-03-21 00:43:37 +0300

    task_9

--- task_9/test.rb
+++ task_9/test.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'accessors.rb'
 class Test
   extend Accessors
   attr_accessor_with_history :b, :c, :d
-end 
+end
 
-#def print_history
-  #puts @history.inspect
-#end
+# def print_history
+# puts @history.inspect
+# end
 test = Test.new
 test.d = 2
 test.d = 3
 test.b = 75
 test.c = 766
-#test.print_history
+# test.print_history
 puts test.c_history.inspect
